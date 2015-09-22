f34306dd022fbf5bb79c88586d7e9fd79804cc60
thecoon <antobarth@gmail.com>
2015-09-23 00:33:06 +0200

    first commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e7/7068839f526100151484ec801fc8b332
@@ -0,0 +1,27 @@
+                                                                                                                    
+
+import java.util.HashMap;
+
+// exemple de main pour le test de l'algorithme de backtrack
+
+public class TP {
+
+	public static void main(String args[]) {
+		//Lire un CSP depuis un fichier
+		String fileName = "example.txt";
+		CSP myProblem;
+		try {    
+			System.out.println("Chargement du fichier : "+new java.io.File( "." ).getCanonicalPath()+"/"+fileName);
+			myProblem = new CSP(fileName);
+		}catch(Exception e) {
+	     System.err.println("Erreur lors du chargement du fichier " + fileName);
+			System.err.println(e);
+			return;
+		}
+		System.out.println("\nRecherche d'une solution au probleme :\n" + myProblem);
+		Solver mySolver = new Solver(myProblem);
+		HashMap<String,Object> mySolution = mySolver.searchSolution();
+		if (mySolution == null) System.out.println("Pas de solution !");
+		else System.out.println("Une solution est " + mySolution);
+	}
+}
---
+++ CSP/src/Glim101/Constraint.java
@@ -0,0 +1,52 @@
+package Glim101;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Constraint {
+	
+	private static int num=0; //pour donner un num�ro unique � chaque contrainte
+	private String name; // nom de la contrainte
+	private ArrayList<String> varTuple; // ensemble ordonn� de variables
+	private Set<ArrayList<Object>> valTuples; // ensemble de tuples de la contrainte
+	
+	public Constraint(ArrayList<String> var) {
+		num++;
+		this.name = "C"+num;
+		varTuple = var; 
+		valTuples = new HashSet<ArrayList<Object>>();
+	}
+	
+	public Constraint(ArrayList<String> var, String name) {
+		num++;
+		this.name = name;
+		varTuple = var; 
+		valTuples = new HashSet<ArrayList<Object>>();
+	}
+	
+	public int getArity() {
+		return varTuple.size();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public ArrayList<String> getVariables() {
+		return varTuple;
+	}
+	
+	public void addTuple(ArrayList<Object> valTuple) {
+		if(valTuple.size() != varTuple.size()) System.err.println("Le tuple " + valTuple + " n'a pas l'arite " + varTuple.size() + " de la contrainte " + name);
+		else if(!valTuples.add(valTuple)) System.err.println("Le tuple " + valTuple + " est deja present dans la contrainte "+ name);
+	}
+	
+	
+	public String toString() {
+		return "\n\t"+ name + " = " + varTuple + " : " + valTuples; 
+	}
+
+
+}
